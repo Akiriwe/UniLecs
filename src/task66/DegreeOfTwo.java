ca9b44e054b69ea12b127303410073c269433c81
Nataliia Manuilova <nataliia_manuilova@epam.com>
2018-01-30 16:45:03 +0200

    added some improvements in order to allow user to input not only 2

--- src/task66/DegreeOfTwo.java
+++ src/task66/DegreeOfTwo.java
@@ -3,6 +3,8 @@ package task66;
 import java.util.Scanner;
 
 public class DegreeOfTwo {
+    private static final int DEGREE_NUMBER = 3;
+
     public static void main(String[] args) {
         System.out.println("Max degree: " + getMaxDegree(getInputString()));
     }
@@ -10,7 +12,7 @@ public class DegreeOfTwo {
     private static StringBuilder getInputString() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input your degrees of two: ");
+        System.out.println("Input your degrees of " + DEGREE_NUMBER + ": ");
         return new StringBuilder().append(scanner.next());
     }
 
@@ -29,13 +31,14 @@ public class DegreeOfTwo {
     }
 
     private static String calculateExpectedPowResultOfCurDegree(int currentDegree) {
-        Integer currentActualResultOfPow = (int) Math.pow(2, currentDegree);
+        Integer currentActualResultOfPow = (int) Math.pow(DEGREE_NUMBER, currentDegree);
         return currentActualResultOfPow.toString();
     }
 
     private static void checkInputDataValidity(String currentDegreeFromString, String currentExpectedResult) {
         if (!currentDegreeFromString.equals(currentExpectedResult)) {
-            throw new WrongSequenceOfDegreesException("Unexpected degree of 2. Check input data.");
+            throw new WrongSequenceOfDegreesException("Unexpected degree result of " + DEGREE_NUMBER
+                    + ". Check input data.");
         }
     }
 }
